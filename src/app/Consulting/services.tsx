8a9684d638110977c61e558cc83d8f4095116e88
Joe Kim <kimjoe1008@gmail.com>
2025-02-16 15:30:49 -0500

    add services

--- src/app/Consulting/services.tsx
+++ src/app/Consulting/services.tsx
@@ -5,7 +5,7 @@ export default function Services(){
         <div className="flex flex-col h-auto w-[90%]">
             <div className="text-3xl font-semibold mb-6">Services</div>
             <div className="text-lg">I provide college admissions coaching and essay coaching to high school students of all ages including:</div>
-            <div className="flex justify-center items-center w-full my-6">
+            <div className="flex justify-center items-center w-full my-10">
                 <div className="w-[90%] items-center">
                     <Image
                         src="/student-studying.jpg"
@@ -17,9 +17,12 @@ export default function Services(){
                 </div>
             </div>
             <div className="">
-                <div className=""></div>
-                <ul className="text-lg">
-                    <li></li>
+                <div className="text-2xl">Private College Counseling</div>
+                <div className="text-lg my-4">For 9th and 10th Graders</div>
+                <ul className="">
+                    <li className="my-1">● Academic advising</li>
+                    <li className="my-1">● Resume building</li>
+                    <li className="my-1">● Diagnostic standardized testing and score assessment</li>
                 </ul>
             </div>
         </div>
