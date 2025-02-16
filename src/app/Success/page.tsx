53cf8540d73b17ee87b3344a713f13dbb8912843
Joe Kim <kimjoe1008@gmail.com>
2025-02-16 03:57:30 -0500

    fix

--- src/app/Success/page.tsx
+++ src/app/Success/page.tsx
@@ -1,12 +1,12 @@
 'use client'
 
 import { Button } from "@heroui/react";
-import Navbar from "../navbar";
+import {NavBar} from "../navbar";
 
 export default function Success(){
     return(
         <main className="flex items-center justify-center min-h-screen flex-col">
-            <Navbar/>
+            <NavBar/>
             <div className="justify-center items-center flex h-full mb-10">
                 <div className="text-4xl">Thank you for contacting me, I will contact you shortly</div>
             </div>
