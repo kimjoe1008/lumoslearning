f694033bf4f2e091de7dd4ae27d8f594a8107540
Joe Kim <kimjoe1008@gmail.com>
2025-02-16 21:22:32 -0500

    fix typo

--- src/app/Success/page.tsx
+++ src/app/Success/page.tsx
@@ -3,12 +3,12 @@
 import { Button } from "@heroui/react";
 import {NavBar} from "../navbar";
 
-export default function success(){
+export default function Success(){
     return(
         <main className="flex items-center justify-center min-h-screen flex-col">
             <NavBar/>
             <div className="justify-center items-center flex h-full mb-10">
-                <div className="text-4xl">Thank you for contacting me, I will contact you shortly</div>
+                <div className="text-4xl">Thank you for reaching out! I'll get back to you soon.</div>
             </div>
             <Button size="lg" href="/" className="text-3xl">Home</Button>
         </main>
