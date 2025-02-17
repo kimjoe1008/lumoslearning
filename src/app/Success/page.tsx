75e8dbca864f2404f7d0378e46d45468fe315714
Joe Kim <kimjoe1008@gmail.com>
2025-02-16 21:25:32 -0500

    fix apostrophe

--- src/app/Success/page.tsx
+++ src/app/Success/page.tsx
@@ -8,7 +8,7 @@ export default function Success(){
         <main className="flex items-center justify-center min-h-screen flex-col">
             <NavBar/>
             <div className="justify-center items-center flex h-full mb-10">
-                <div className="text-4xl">Thank you for reaching out! I'll get back to you soon.</div>
+                <div className="text-4xl">Thank you for reaching out! I&apos;ll get back to you soon.</div>
             </div>
             <Button size="lg" href="/" className="text-3xl">Home</Button>
         </main>
