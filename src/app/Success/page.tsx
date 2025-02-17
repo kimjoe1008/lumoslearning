1c9b2750eaade2c342bc4f62ea593ca6a7d3b8f7
Joe Kim <kimjoe1008@gmail.com>
2025-02-16 21:39:38 -0500

    fix success page

--- src/app/Success/page.tsx
+++ src/app/Success/page.tsx
@@ -3,14 +3,17 @@
 import { Button } from "@heroui/react";
 import {NavBar} from "../navbar";
 
-export default function Success(){
-    return(
-        <main className="flex items-center justify-center min-h-screen flex-col">
-            <NavBar/>
-            <div className="justify-center items-center flex h-full mb-10">
-                <div className="text-4xl">Thank you for reaching out! I&apos;ll get back to you soon.</div>
+export default function Success() {
+    return (
+        <div className="min-h-screen flex flex-col">
+            <NavBar />
+
+            <div className="flex-1 flex flex-col items-center justify-center">
+                <div className="text-4xl text-center mb-6">
+                    Thank you for reaching out! I&apos;ll get back to you soon.
+                </div>
+                <Button size="lg" href="/" className="text-2xl bg-white font-semibold">Home</Button>
             </div>
-            <Button size="lg" href="/" className="text-3xl">Home</Button>
-        </main>
+        </div>
     );
 }
